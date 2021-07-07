dfc5939954d3e55bc8ed170f949f09170880dc10
Carolyn Rehm <carolyn.rehm@revature.com>
2021-07-07 11:51:35 -0500

    project setup

---
+++ src/main/java/dev/rehm/servlets/ActorServlet.java
@@ -0,0 +1,25 @@
+package dev.rehm.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ActorServlet extends HttpServlet {
+
+    public ActorServlet() {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp); this would just return a 405 status code
+
+        // obtain list of actor objects from database or other source
+
+        // convert actor objects to json (Jackson databind)
+
+        // use the printwriter to write json to response body
+
+    }
+}
